593698ddf37054fcbca232a04521cf16e42ba7aa
wangsy <wangsy@bjhetang.com>
2016-06-04 10:01:08 +0800

    测试用例整理

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,15 +1,23 @@
 DROP TABLE IF EXISTS employee;
 
-CREATE TABLE employee(
-    id	   BIGINT UNSIGNED           NOT NULL AUTO_INCREMENT ,
-    name     VARCHAR(32)           	       NULL 			 ,
-    password     VARCHAR(32)           	       NULL 		,
-    email	 VARCHAR(32)                NULL 	         ,
-    role	 BIGINT UNSIGNED              NULL 	    ,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
+CREATE TABLE employee (
+  id       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name     VARCHAR(32)     NULL,
+  password VARCHAR(32)     NULL,
+  email    VARCHAR(32)     NULL,
+  role     BIGINT UNSIGNED NULL,
+  PRIMARY KEY (id)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
 
-insert into employee (`name`,`password`,email,role) values("wangsy","12345,","2234wwe@163.com","1");
-select  * from employee;
+INSERT INTO employee (`name`, `password`, email, role) VALUES
+  ("wangsy11", "1~2(3)4&*5$", "2234wwe@163.com", "1"),
+  ("wangsy22", "1~2(3)4&*5$", "2234wwe@163.com", "1"),
+  ("wangsy33", "1~2(3)4&*5$", "2234wwe@163.com", "1"),
+  ("wangsy44", "1~2(3)4&*5$", "2234wwe@163.com", "1"),
+  ("wangsy55", "1~2(3)4&*5$", "2234wwe@163.com", "1"),
+  ("wangsy66", "1~2(3)4&*5$", "2234wwe@163.com", "1");
+SELECT * FROM employee;
 
 
